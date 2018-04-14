07c8e937a5ebf1fcab5f2aef25c37abf7f57d240
Ashwinee Panda <kiddyboots216@Ashwinees-MacBook-Pro.local>
2018-04-14 13:24:38 -0700

    Add inference to GAN model

--- models/gan.py
+++ models/gan.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import os
+import pickle
 
 import numpy as np
+import nltk
 from keras.layers import Input, Embedding, LSTM, Dense, RepeatVector, Dropout, merge
 from keras.optimizers import Adam 
 from keras.models import Model
@@ -18,67 +20,26 @@ class ConversationalNetwork:
     def __init__(self):
         self.gen_initial_weights_path = os.path.join(
             os.path.dirname(__file__), 'pretrained/my_model_weights.h5')
+        self.vocabulary_file = os.path.join(
+            os.path.dirname(__file__), 'pretrained/vocabulary_movie')
+        self.file_saved_context = os.path.join(
+            os.path.dirname(__file__), 'pretrained/saved_context')
+        self.file_saved_answer = os.path.join(
+            os.path.dirname(__file__), 'pretrained/saved_answer')
+
+        self.vocabulary = pickle.load(open(self.vocabulary_file, 'rb'))
+        self.name_of_computer = 'ben'
+
         self.learning_rate = 0.000001
         self.maxlen_input = 50
         self.sentence_embedding_size = 300
         self.word_embedding_size = 100
         self.dictionary_size = 7000
 
-    def preprocess_input(self, raw_word, name):   
-        l1 = ['won’t','won\'t','wouldn’t','wouldn\'t','’m', '’re', '’ve', '’ll', '’s','’d', 'n’t', '\'m', '\'re', '\'ve', '\'ll', '\'s', '\'d', 'can\'t', 'n\'t', 'B: ', 'A: ', ',', ';', '.', '?', '!', ':', '. ?', ',   .', '. ,', 'EOS', 'BOS', 'eos', 'bos']
-        l2 = ['will not','will not','would not','would not',' am', ' are', ' have', ' will', ' is', ' had', ' not', ' am', ' are', ' have', ' will', ' is', ' had', 'can not', ' not', '', '', ' ,', ' ;', ' .', ' ?', ' !', ' :', '? ', '.', ',', '', '', '', '']
-        l3 = ['-', '_', ' *', ' /', '* ', '/ ', '\"', ' \\"', '\\ ', '--', '...', '. . .']
-        l4 = ['jeffrey','fred','benjamin','paula','walter','rachel','andy','helen','harrington','kathy','ronnie','carl','annie','cole','ike','milo','cole','rick','johnny','loretta','cornelius','claire','romeo','casey','johnson','rudy','stanzi','cosgrove','wolfi','kevin','paulie','cindy','paulie','enzo','mikey','i\97','davis','jeffrey','norman','johnson','dolores','tom','brian','bruce','john','laurie','stella','dignan','elaine','jack','christ','george','frank','mary','amon','david','tom','joe','paul','sam','charlie','bob','marry','walter','james','jimmy','michael','rose','jim','peter','nick','eddie','johnny','jake','ted','mike','billy','louis','ed','jerry','alex','charles','tommy','bobby','betty','sid','dave','jeffrey','jeff','marty','richard','otis','gale','fred','bill','jones','smith','mickey']    
-
-        raw_word = raw_word.lower()
-        raw_word = raw_word.replace(', ' + name_of_computer, '')
-        raw_word = raw_word.replace(name_of_computer + ' ,', '')
-
-        for j, term in enumerate(l1):
-            raw_word = raw_word.replace(term,l2[j])
-            
-        for term in l3:
-            raw_word = raw_word.replace(term,' ')
-        
-        for term in l4:
-            raw_word = raw_word.replace(', ' + term, ', ' + name)
-            raw_word = raw_word.replace(' ' + term + ' ,' ,' ' + name + ' ,')
-            raw_word = raw_word.replace('i am ' + term, 'i am ' + name_of_computer)
-            raw_word = raw_word.replace('my name is' + term, 'my name is ' + name_of_computer)
-        
-        for j in range(30):
-            raw_word = raw_word.replace('. .', '')
-            raw_word = raw_word.replace('.  .', '')
-            raw_word = raw_word.replace('..', '')
-           
-        for j in range(5):
-            raw_word = raw_word.replace('  ', ' ')
-            
-        if raw_word[-1] !=  '!' and raw_word[-1] != '?' and raw_word[-1] != '.' and raw_word[-2:] !=  '! ' and raw_word[-2:] != '? ' and raw_word[-2:] != '. ':
-            raw_word = raw_word + ' .'
-        
-        if raw_word == ' !' or raw_word == ' ?' or raw_word == ' .' or raw_word == ' ! ' or raw_word == ' ? ' or raw_word == ' . ':
-            raw_word = 'what ?'
-        
-        if raw_word == '  .' or raw_word == ' .' or raw_word == '  . ':
-            raw_word = 'i do not want to talk about it .'
-          
-        return raw_word
-
-    def tokenize(self, sentences):
-        # Tokenizing the sentences into words:
-        tokenized_sentences = nltk.word_tokenize(sentences)
-        index_to_word = [x[0] for x in vocabulary]
-        word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
-        tokenized_sentences = [w if w in word_to_index else unknown_token for w in tokenized_sentences]
-        X = np.asarray([word_to_index[w] for w in tokenized_sentences])
-        s = X.size
-        Q = np.zeros((1,self.maxlen_input))
-        if s < (self.maxlen_input + 1):
-            Q[0,- s:] = X
-        else:
-            Q[0,:] = X[- self.maxlen_input:]
-        return Q
+        self.state = {
+            'probability': 0.0,
+            'text': ''
+        }
 
     def build_model(self, is_retraining):
         def _build_training():
@@ -122,7 +83,7 @@ class ConversationalNetwork:
 
         #     # 2. Build discriminator network with initial weights.
         #     # WON'T IMPLEMENT YET
-              
+        
         if is_retraining:
             # NOTE: doesn't actually build a discriminator.
             _build_generator_only()
@@ -132,6 +93,23 @@ class ConversationalNetwork:
 
         self.load_initial_weights()
         return self.generator
+
+    def run_inference(self, input_text, name):
+        input_text = input_text.strip()
+        preprocessed_input = self._preprocess_input(input_text, self.name_of_computer)
+
+        if self.state["probability"] > 0.2:
+            query = self.state['text'] + ' ' + preprocessed_input
+        else:    
+            query = preprocessed_input
+        Q = self._tokenize(query)
+
+        predout, self.state["probability"] = self._greedy_decoder(Q[0:1])
+        start_index = predout.find('EOS')
+        self.state['text'] = self._preprocess_input(predout[0:start_index], name) + ' EOS'
+
+        best_answer = self.state["text"][0 : -4]
+        return best_answer
 
     def load_initial_weights(self):
         self.load_weights(self.gen_initial_weights_path)
@@ -160,16 +138,97 @@ class ConversationalNetwork:
             new_weights.append(w / factor)
         return new_weights
 
-# if __name__ == '__main__':
-#     m = ConversationalNetwork()
-#     m.build_model(False)
-    # w = m.get_weights()
-    # print(w[0][:1])
-    # w2 = m.scale_weights(w, 2)
-    # print(w2[0][:1])
-    # w3 = m.inverse_scale_weights(w2, 2)
-    # print(w3[0][:1])
-    # w4 = m.sum_weights(w, w)
-    # print(w4[0][:1])
+    def _preprocess_input(self, raw_word, name):   
+        l1 = ['won’t','won\'t','wouldn’t','wouldn\'t','’m', '’re', '’ve', '’ll', '’s','’d', 'n’t', '\'m', '\'re', '\'ve', '\'ll', '\'s', '\'d', 'can\'t', 'n\'t', 'B: ', 'A: ', ',', ';', '.', '?', '!', ':', '. ?', ',   .', '. ,', 'EOS', 'BOS', 'eos', 'bos']
+        l2 = ['will not','will not','would not','would not',' am', ' are', ' have', ' will', ' is', ' had', ' not', ' am', ' are', ' have', ' will', ' is', ' had', 'can not', ' not', '', '', ' ,', ' ;', ' .', ' ?', ' !', ' :', '? ', '.', ',', '', '', '', '']
+        l3 = ['-', '_', ' *', ' /', '* ', '/ ', '\"', ' \\"', '\\ ', '--', '...', '. . .']
+        l4 = ['jeffrey','fred','benjamin','paula','walter','rachel','andy','helen','harrington','kathy','ronnie','carl','annie','cole','ike','milo','cole','rick','johnny','loretta','cornelius','claire','romeo','casey','johnson','rudy','stanzi','cosgrove','wolfi','kevin','paulie','cindy','paulie','enzo','mikey','i\97','davis','jeffrey','norman','johnson','dolores','tom','brian','bruce','john','laurie','stella','dignan','elaine','jack','christ','george','frank','mary','amon','david','tom','joe','paul','sam','charlie','bob','marry','walter','james','jimmy','michael','rose','jim','peter','nick','eddie','johnny','jake','ted','mike','billy','louis','ed','jerry','alex','charles','tommy','bobby','betty','sid','dave','jeffrey','jeff','marty','richard','otis','gale','fred','bill','jones','smith','mickey']    
 
+        raw_word = raw_word.lower()
+        raw_word = raw_word.replace(', ' + self.name_of_computer, '')
+        raw_word = raw_word.replace(self.name_of_computer + ' ,', '')
+
+        for j, term in enumerate(l1):
+            raw_word = raw_word.replace(term,l2[j])
+            
+        for term in l3:
+            raw_word = raw_word.replace(term,' ')
+        
+        for term in l4:
+            raw_word = raw_word.replace(', ' + term, ', ' + name)
+            raw_word = raw_word.replace(' ' + term + ' ,' ,' ' + name + ' ,')
+            raw_word = raw_word.replace('i am ' + term, 'i am ' + self.name_of_computer)
+            raw_word = raw_word.replace('my name is' + term, 'my name is ' + self.name_of_computer)
+        
+        for j in range(30):
+            raw_word = raw_word.replace('. .', '')
+            raw_word = raw_word.replace('.  .', '')
+            raw_word = raw_word.replace('..', '')
+           
+        for j in range(5):
+            raw_word = raw_word.replace('  ', ' ')
+            
+        if raw_word[-1] !=  '!' and raw_word[-1] != '?' and raw_word[-1] != '.' and raw_word[-2:] !=  '! ' and raw_word[-2:] != '? ' and raw_word[-2:] != '. ':
+            raw_word = raw_word + ' .'
+        
+        if raw_word == ' !' or raw_word == ' ?' or raw_word == ' .' or raw_word == ' ! ' or raw_word == ' ? ' or raw_word == ' . ':
+            raw_word = 'what ?'
+        
+        if raw_word == '  .' or raw_word == ' .' or raw_word == '  . ':
+            raw_word = 'i do not want to talk about it .'
+          
+        return raw_word
+
+    def _tokenize(self, sentences):
+        # Tokenizing the sentences into words:
+        tokenized_sentences = nltk.word_tokenize(sentences)
+        index_to_word = [x[0] for x in self.vocabulary]
+        word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+        tokenized_sentences = [w if w in word_to_index else unknown_token for w in tokenized_sentences]
+        X = np.asarray([word_to_index[w] for w in tokenized_sentences])
+        s = X.size
+        Q = np.zeros((1,self.maxlen_input))
+        if s < (self.maxlen_input + 1):
+            Q[0,- s:] = X
+        else:
+            Q[0,:] = X[- self.maxlen_input:]
+        return Q
+
+    def _greedy_decoder(self, input):
+        flag = 0
+        prob = 1
+        ans_partial = np.zeros((1,self.maxlen_input))
+        ans_partial[0, -1] = 2  #  the index of the symbol BOS (begin of sentence)
+        for k in range(self.maxlen_input - 1):
+            ye = self.generator.predict([input, ans_partial])
+            yel = ye[0,:]
+            p = np.max(yel)
+            mp = np.argmax(ye)
+            ans_partial[0, 0:-1] = ans_partial[0, 1:]
+            ans_partial[0, -1] = mp
+            if mp == 3:  #  he index of the symbol EOS (end of sentence)
+                flag = 1
+            if flag == 0:    
+                prob = prob * p
+        text = ''
+        for k in ans_partial[0]:
+            k = k.astype(int)
+            if k < (self.dictionary_size-2):
+                w = self.vocabulary[k]
+                text = text + w[0] + ' '
+        return (text, prob)
+
+if __name__ == '__main__':
+    m = ConversationalNetwork()
+    m.build_model(is_retraining=False)
+
+    print("\n \n \n \n    CHAT:     \n \n")
+    print('computer: hi ! please type your name.\n')
+    name = input('user: ')
+    print('computer: hi , ' + name +' ! My name is ' + m.name_of_computer + '.\n') 
+
+    while True:
+        utterance = input('user: ')
+        response = m.run_inference(utterance, name)
+        print('computer: ' + response + '\n\n\n')
 
