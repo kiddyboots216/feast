2235ef10a0a5d991e9107e05f44e737015e34c30
nzoghb <nzoghb@berkeley.edu>
2018-04-14 11:13:28 -0700

    ipfs utils added

--- blockchain/Master.sol
+++ blockchain/Master.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.4.21;
 
-import "Query.sol";
+import "blockchain/Query.sol";
 
 contract Master {
     address public owner;
@@ -9,7 +9,8 @@ contract Master {
     mapping(address => uint256[]) public queries;
     mapping(uint256 => address) private addressBook;
 
-    address q;
+    address public q;
+    address[] public addrList;
 
     event QueryCreated(address,address);
 
@@ -19,19 +20,17 @@ contract Master {
 
     function query(address target)
         public
-        payable
     {
         int[] memory lst = new int[](3);
         lst[0] = 0;
         lst[1] = 1;
         lst[2] = 2;
-        q = address(new Query(3, lst, 0, 0, 5, lst, 3));
-        address[] memory addrlst = new address[](1);
-        addrlst[0] = target;
-        pingClients(addrlst);
+        addrList.push(target);
+        q = address(new Query(3, lst, 0, 0, 5, lst, 3, addrList));
+        pingClients(addrList);
     }
 
-    function pingClients(address[] clientList) internal {
+    function pingClients(address[] clientList) {
         uint clientLen = clientList.length;
         for (uint i = 0; i < clientLen; i++) {
             emit QueryCreated(clientList[i], q);
--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import logging
 import shutil
 import time
 
+import asyncio
 import numpy as np
 import tensorflow as tf
 
@@ -10,42 +11,67 @@ from models.cnn import CNN
 from models.lstm import LSTM
 
 from eth_utils import is_address
+from solc import compile_source, compile_files
 from blockchain.blockchain_utils import *
 
 
 class Client(object):    
     def __init__(self, iden, X_train, y_train, provider, masterAddress=None, clientAddress=None):
         
+        self.web3 = provider
+
         self.PASSPHRASE = 'panda'
-        self.TEST_ACCOUNT = '0xf6419f5c5295a70C702aC21aF0f64Be07B59F3c4'
+        self.TEST_ACCOUNT = '0xb4734dCc08241B46C0D7d22D163d065e8581503e'
         self.TEST_KEY = '146396092a127e4cf6ff3872be35d49228c7dc297cf34da5a0808f29cf307da1'
+
+        # import_acct = self.web3.personal.importRawKey(self.TEST_KEY, self.PASSPHRASE)
+
+        # assert(import_acct == self.TEST_ACCOUNT)
+
+        contract_source_path_A = "blockchain/Master.sol"
+        contract_source_path_B = "blockchain/Query.sol"
+        self.compiled_sol = compile_files([contract_source_path_A, contract_source_path_B])
 
         self.iden = iden
         self.X_train = X_train
         self.y_train = y_train
-        self.web3 = provider
-        if masterAddress:
-            assert(is_address(masterAddress))
-            self.masterContract = self.web3.eth.contract(
-                address=masterAddress,
-                abi=contractAbi)
-        else:
-            #TODO: Figure out what to do in event that a master address is not supplied
 
-            self.masterContract = None
         if clientAddress:
             assert(is_address(clientAddress))
             self.clientAddress = clientAddress
         else:
             #TODO: Initialize client 'container' address if it wasn't assigned one
-            PASSPHRASE = 'panda'
             self.clientAddress = self.web3.personal.newAccount(self.PASSPHRASE)
             assert(is_address(self.clientAddress))
+            self.web3.personal.unlockAccount(self.clientAddress, self.PASSPHRASE)
             # send_raw_tx(self.web3, self.clientAddress, self.TEST_ACCOUNT, self.TEST_KEY)
             # self.web3.eth.sendTransaction({"from": self.TEST_ACCOUNT, "to": self.clientAddress,
                                            # "value": 9999999999})
-        get_testnet_eth(self.clientAddress)
-        self.buyerContract = None          
+        print("Client Address:", self.clientAddress)
+
+        self.Master_address = masterAddress
+    
+    def get_money(self):
+        get_testnet_eth(self.clientAddress, self.web3)
+        print(self.web3.eth.getBalance(self.clientAddress))
+
+        if self.Master_address:
+            assert(is_address(self.Master_address))
+        else:
+            #TODO: Figure out what to do in event that a master address is not supplied
+            Query_id, self.Query_interface = self.compiled_sol.popitem()
+            Master_id, self.Master_interface = self.compiled_sol.popitem()
+
+            # self.Query_address = deploy_Query(self.web3, self.Query_interface, self.TEST_ACCOUNT, addr_lst)
+            self.Master_address = deploy_Master(self.web3, self.Master_interface, self.clientAddress)
+
+    def ping_client(self, query_address):
+        contract_obj = web3.eth.contract(
+           address=query_address,
+           abi=self.Query_interface)
+        tx_hash = contract_obj.functions.pingClients(addr_lst).transact({'from': self.clientAddress})
+        tx_receipt = web3.eth.getTransactionReceipt(tx_hash)
+        return tx_receipt
 
     def setup_model(self, model_type):
         self.model_type = model_type
@@ -110,12 +136,10 @@ class Client(object):
     #     log = contract_obj.events.ResponseReceived().processReceipt(tx_receipt)
     #     return log[0]
 
-    def handle_event(self, event):
-        print(event)
-
     def handle_ClientSelected_event(self, event):
         #TODO: get weights and config from event
-        #weights will come from the smart contract's variable currentWeights[]
+        #weights will come from the smart contract's variable currentWeights[]        
+
         weights = event.get_weights()
         #will be hardcoded
         config = {
@@ -138,35 +162,82 @@ class Client(object):
         log = contract_obj.events.ResponseReceived().processReceipt(tx_receipt)
         return log[0]
 
-    def handle_QueryCreated_event(self, event):
-        #TODO: get address of the buyer from the master contract
-        address = event.get_address()
+    def handle_QueryCreated_event(self, event_data):
+        print(event_data)
+        address = event_data.split("000000000000000000000000")[2]
         assert(is_address(address))
-        self.buyerAddress = adress
-        start_listening(buyerAddress = self.buyerAddress)
+        self.buyerAddress = address
+        return event_data.split("000000000000000000000000")
+        # start_listening(buyerAddress = self.buyerAddress)
 
-    def start_listening(self, buyerAddress = None, 
-        # event_to_listen = None, 
-        poll_interval = 1000):
+    # def start_listening(self, buyerAddress = None, 
+    #     # event_to_listen = None, 
+    #     poll_interval = 1000):
+    #     #this should set this client to start listening to a specific contract
+    #     #make this non-blocking
+    #     #TODO: Make event filtering work!
+    #     if buyerAddress:
+    #         assert(is_address(buyerAddress))
+    #         self.buyerContract = self.web3.eth.contract(
+    #                 address=buyerAddress,
+    #                 abi=contractAbi)
+    #         event_filter = self.buyerContract.eventFilter('ClientSelected', {'fromBlock': 'latest'})
+    #         while True:
+    #             for event in event_filter.get_new_entries():
+    #                 handle_clientSelected_event(event)
+    #             time.sleep(poll_interval)
+    #     else:
+    #         event_filter = self.masterContract.eventFilter('QueryCreated', {'fromBlock': 'latest'})
+    #         while True:
+    #             for event in event_filter.get_new_entries():
+    #                 handle_queryCreated_event(event)
+    #             time.sleep(poll_interval)
+
+
+    async def start_listening(self, event_to_listen, poll_interval=5):
         #this should set this client to start listening to a specific contract
-        #make this non-blocking
-        #TODO: Make event filtering work!
-        if buyerAddress:
-            assert(is_address(buyerAddress))
-            self.buyerContract = self.web3.eth.contract(
-                    address=buyerAddress,
-                    abi=contractAbi)
-            event_filter = self.buyerContract.eventFilter('ClientSelected', {'fromBlock': 'latest'})
-            while True:
-                for event in event_filter.get_new_entries():
-                    handle_clientSelected_event(event)
-                time.sleep(poll_interval)
+    #     assert(is_address(address))
+        while True:
+            lst = event_to_listen.get_new_entries()
+            if lst:
+                print(lst[0])
+                return lst[0]
+            await asyncio.sleep(poll_interval)
+
+    def filter_set(self, event_sig, contract_addr, handler):
+        event_signature_hash = self.web3.sha3(text=event_sig).hex()
+        event_filter = self.web3.eth.filter({
+            "address": contract_addr.lower(),
+            "topics": [event_signature_hash]
+            })
+        asyncio.set_event_loop(asyncio.new_event_loop())
+        loop = asyncio.get_event_loop()
+        try:
+            inter = loop.run_until_complete(
+                self.start_listening(event_filter, 2))
+            check = handler(inter['data'])
+        finally:
+            loop.close()
+        return check
+
+    def main(self, master_address):
+        check = self.filter_set("QueryCreated(address,address)", master_address, self.handle_QueryCreated_event)
+        if check[0] + check[1] == self.clientAddress.lower():
+            # retrieve new address and rerun loop
+            target_contract = check[0] + check[2]
+            print(target_contract)
+            retval = self.filter_set("ClientSelected(address)", target_contract, self.handle_ClientSelected_event)
+            return "I got chosen:", retval[0] + retval[1]
         else:
-            event_filter = self.masterContract.eventFilter('QueryCreated', {'fromBlock': 'latest'})
-            while True:
-                for event in event_filter.get_new_entries():
-                    handle_queryCreated_event(event)
-                time.sleep(poll_interval)
+            return "failure"
+
+
+
+
+
+
+
+
 
     def get_checkpoints_folder(self):
         return "./checkpoints-{0}/{1}/".format(self.iden, self.model_type)
