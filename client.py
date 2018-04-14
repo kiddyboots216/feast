2c7f4455449045e13b691d8935d36735ab2304c7
Georgy Marrero <georgymarrero@berkeley.edu>
2018-04-14 15:15:02 -0700

    coordinated with client.py

--- client.py
+++ client.py
@@ -18,7 +18,7 @@ from blockchain.ipfs_utils import *
 
 class Client(object):
     def __init__(self, iden, X_train, y_train, provider, delegatorAddress=None, clientAddress=None):
-        
+
         self.web3 = provider
         self.api = ipfsapi.connect('127.0.0.1', 5001)
 
@@ -100,38 +100,59 @@ class Client(object):
             raise ValueError("Model {0} not supported.".format(model_type))
         self.weights_metadata = self.model.get_weights_shape()
 
-    def train(self, config, weights=None):
-        time.sleep(10)
-        return
+    def train(self, config):
         #TODO: Make train() only need to take in the config argument ONCE
         logging.info('Training just started.')
         assert weights != None, 'weights must not be None.'
-        batch_size = self.X_train.shape[0] if config["batch_size"] == -1 \
-            else config["batch_size"]
-        epochs = config["epochs"]
-        learning_rate = config["learning_rate"]
-        params = {'new_weights': weights, 'learning_rate': learning_rate}
 
-        classifier = tf.estimator.Estimator(
-            model_fn=self.model.get_model,
-            model_dir=self.get_checkpoints_folder(),
-            params = params
-        )
+        # Get weights from IPFS and load into model
+        ipfs2keras(self.model, self.model_address) # fix me
+
+        # Get training data
+        q_train = ...
+        a_train = ...
+
+        # Train model
+        self.model.train_model(config, q_train, a_train)
+
+        # Prepare federated learning return
+        n_k = a_train.shape[0]
+        # new_weights = self.model.get_weights()
+
+        # Save new weights to IPFS and return address
+        new_model_address = keras2ipfs(self.model)
+
+        return new_model_address, n_k
+
+
+
+
+        # batch_size = self.X_train.shape[0] if config["batch_size"] == -1 \
+        #     else config["batch_size"]
+        # epochs = config["epochs"]
+        # learning_rate = config["learning_rate"]
+        # params = {'new_weights': weights, 'learning_rate': learning_rate}
+
+        # classifier = tf.estimator.Estimator(
+        #     model_fn=self.model.get_model,
+        #     model_dir=self.get_checkpoints_folder(),
+        #     params = params
+        # )
         # tensors_to_log = {"probabilities": "softmax_tensor"}
         # logging_hook = tf.train.LoggingTensorHook(
         #     tensors=tensors_to_log, every_n_iter=50)
-        train_input_fn = tf.estimator.inputs.numpy_input_fn(
-            x={"x": self.X_train},
-            y=self.y_train,
-            batch_size=batch_size,
-            num_epochs=epochs,
-            shuffle=True
-        )
-        classifier.train(
-            input_fn=train_input_fn,
-            # steps=1
-            # hooks=[logging_hook]
-        )
+        # train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        #     x={"x": self.X_train},
+        #     y=self.y_train,
+        #     batch_size=batch_size,
+        #     num_epochs=epochs,
+        #     shuffle=True
+        # )
+        # classifier.train(
+        #     input_fn=train_input_fn,
+        #     # steps=1
+        #     # hooks=[logging_hook]
+        # )
         logging.info('Training complete.')
         new_weights = self.model.get_weights(self.get_latest_checkpoint())
         shutil.rmtree("./checkpoints-{0}/".format(self.iden))
