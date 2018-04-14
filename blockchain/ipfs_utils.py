32b0f7ede4380056ff0e0a6621d6459d2c4062ae
nzoghb <nzoghb@berkeley.edu>
2018-04-14 11:44:24 -0700

    changes to ipfs

--- blockchain/ipfs_utils.py
+++ blockchain/ipfs_utils.py
@@ -5,21 +5,21 @@ import ipfsapi
 api = ipfsapi.connect('127.0.0.1', 5001)
 
 def serialize_keras_model(destination='model_weights.h5'):
-    model.save(destination)
     with open(destination, 'rb') as f:
         model_bin = f.read()
         f.close()
-return model_bin
+    return model_bin
 
 def deserialize_keras_model(content, destination='model_weights.h5'):
     with open(destination, 'wb') as g:
         g.write(content)
         g.close()
     model = keras.models.load_model(destination)
-return model
+    return model
 
-def keras_to_ipfs(api, model):
-    return api.add(serialize_keras_model(model))
+def keras_to_ipfs(api, model='model_weights.h5'):
+    # return api.add(serialize_keras_model(model))
+    return api.add(model)
 
 
 def ipfs_to_keras(api, model_addr):
